b3aea93df5d688c14b26b5a8c9f48c5ae3eb12b2
tanner466 <tanner466@gmail.com>
2016-02-11 13:38:05 -0500

    Finish logo problem, add provided proect files.

--- GoogleLogo/ChangeLogo.js
+++ GoogleLogo/ChangeLogo.js
@@ -3,3 +3,9 @@ var logo = document.getElementById("lga");
 var styles = logo.getElementsByTagName("style");
 
 styles[0].innerHTML = "#hplogo{background:url(http://www.underconsideration.com/brandnew/archives/yahoo_logo_detail.png) no-repeat}@media (-webkit-max-device-pixel-ratio:1),(max-resolution:96dpi){#hplogo{background:url(/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png) no-repeat}}"
+
+var buttonClass = document.getElementsByClassName('jsb');
+
+var inputs = buttonClass[0].getElementsByTagName('input');
+
+inputs[0].value = "Yahoo";
---
+++ jquery_fundamentals/jquery_exercises.js
@@ -0,0 +1,90 @@
+// SOLUTIONS GO BELOW EACH EXERCISE
+
+// STEP 0: Document Needs to be ready first!
+// Ensure that all the code you'll write below only execute when the document is ready
+// Hint: you can use .ready on the document object
+
+
+// STEP 1: Change Background
+
+// Change the background color of '#step-1' by script.
+// Hint: you can use .css
+
+
+// STEP 2: Change Parent
+
+// Change the text in the span, a child of "#step-2"
+// Hint: you can use .text
+
+
+// STEP 3: Create Clone
+
+// Create a clone of the span in "#step-3" and position it under the original one.
+// Hint: you can use .clone and .insertAfter
+
+
+// STEP 4: Use Filter
+
+// Change background color of the second ".target"
+// Hint: you can use .eq and .css
+
+
+// STEP 5: Disable Buttons
+
+// Disable the button
+// Hint: you will have to select the button and can use .attr
+
+
+// STEP 6: Uncheck CheckBoxes
+
+// Uncheck all checkboxes using jQuery
+// Hint: you will have to select the input and can use .removeAttr
+
+
+// STEP 7: Change Parent
+
+// Move "#child-7" from "#step-7a" to "#step-7b"
+// Hint: you can use .appendTo
+
+
+// STEP 8: Change Size
+
+// Double the size of "#step-8"
+// Hint: you can use .css, .width and .height
+
+
+// STEP 9: Empty Elements
+
+// Remove all children and text of "#step-9"
+// Hint: you can use .empty
+
+
+// STEP 10: Delay
+
+// Show Alert with 1 second delay (Use "setTimeout")
+// Hint: you can use setTimeout and alert (these aren't jQuery functions!)
+
+
+// STEP 11: Count
+
+// Show the number of children in an alert
+// Hint: you can use .children, .length and alert or you could use .find, .size and alert. the alert should show 5
+
+
+// STEP 12: Animate
+
+// Make "#step-12" double size with animation
+// Hint: you can use .animate, .width, and .height
+
+
+// STEP 13: All But One
+
+// Remove all children of "#step-13" other than any h4 tags
+// Hint: you can use .children, .not and .remove
+
+
+// STEP 14: Without Children
+
+// Remove all 'div's from "#step-14" which have no child elements.
+// Hint: you can use .not and .remove. Look closely at what options you can pass .not
+
